63738ff729930bb4c4141dfe84f808164005762c
jorg3uceda-gif <jorg3uceda@gmail.com>
2026-03-02 15:22:05 -0600

    agregue tool bar, clase de variables

---
+++ src/proyectofinalp2_jorgeeuceda/Variable.java
@@ -0,0 +1,36 @@
+package proyectofinalp2_jorgeeuceda;
+
+public class Variable {
+    
+    String name;
+    String type;
+
+    public Variable(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "("+type+")"+" "+name;
+    }
+    
+    
+    
+}
